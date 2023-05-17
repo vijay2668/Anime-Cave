f28cb9a6e74b090aa6aea9c0718b25072777adbb
vijay2668 <121243884+vijay2668@users.noreply.github.com>
2023-05-17 11:26:31 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ import Herosection from '../components/Herosection'
 
 const About = () => {
   return (
-    <Herosection title={"OUR STORY"} imageUrl={"/about1.svg"} />
+    <Herosection title={"OUR STORY"} imageUrl={"/home.svg"} />
   )
 }
 
